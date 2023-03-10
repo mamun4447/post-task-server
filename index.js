2cd3fe933d15b32e85c954e5e887232b39e87efc
mamun4447 <abdullah15-12557@diu.edu.bd>
2023-03-10 11:27:42 +0600

    some functionality completed

---
+++ index.js
@@ -0,0 +1,108 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser());
+app.use(express());
+app.use(cors());
+
+//===MongoDB===//
+// UQpn4YPmaciFztzn;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { success } = require("daisyui/src/colors");
+const uri =
+  "mongodb+srv://userdashboard:EhgKjYptDK9kblMf@cluster0.otp6uvz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+//=====Function to connect Mongo======//
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected!");
+  } catch (error) {
+    console.error(error);
+  }
+}
+run().catch((error) => console.error(error));
+
+//====database collections=====//
+const usersCollection = client.db("contentPoster").collection("users");
+
+//===Post User===//
+app.post("/users", async (req, res) => {
+  try {
+    await usersCollection.insertOne(req.body);
+    res.send({
+      success: true,
+      message:"Successfully created user account!"
+    })
+  } catch (error) {
+    console.log(error);
+    res.send({success:false,error:error.message})
+  }
+})
+
+//===Post Content===//
+app.post("/content", async (req, res) => {
+  try {
+    await usersCollection.insertOne(req.body);
+    res.send({
+      success: true,
+      message:"Successfully created a post!"
+    })
+  } catch (error) {
+    console.log(error);
+    res.send({success:false,error:error.message})
+  }
+})
+
+//===Post Content===//
+app.get("/content", async (req, res) => {
+  try {
+    const result=await usersCollection.find({}).toArray();
+    res.send({
+      success: true,
+      data:result
+    })
+  } catch (error) {
+    console.log(error);
+    res.send({success:false,error:error.message})
+  }
+})
+
+
+//=====Delete review=====//
+app.delete("/content/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+     await usersCollection.deleteOne(query);
+    res.send({success:true,message:"Successfully deleted!"});
+  } catch (error) {
+      console.log(error)
+      res.send({
+        
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+
+
+
+//===> Initial section <===//
+app.get("/", (req, res) => {
+    res.send("Server is running!!")
+})
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
