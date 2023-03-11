2540e7795bd61ee8425440740d2e8b447cc2ace4
mamun4447 <abdullah15-12557@diu.edu.bd>
2023-03-11 15:24:57 +0600

    post content, get connent, get spacific content

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ run().catch((error) => console.error(error));
 
 //====database collections=====//
 const usersCollection = client.db("contentPoster").collection("users");
+const contentCollection = client.db("contentPoster").collection("content");
 
 //===Post User===//
 app.post("/users", async (req, res) => {
@@ -42,54 +43,82 @@ app.post("/users", async (req, res) => {
     await usersCollection.insertOne(req.body);
     res.send({
       success: true,
-      message:"Successfully created user account!"
-    })
+      message: "Successfully created user account!",
+    });
   } catch (error) {
     console.log(error);
-    res.send({success:false,error:error.message})
+    res.send({ success: false, error: error.message });
   }
-})
+});
+
+//==Get User===//
+app.get("/users/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const result = await usersCollection.findOne({ email: email });
+    res.send({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({ success: false, error: error.message });
+  }
+});
 
 //===Post Content===//
 app.post("/content", async (req, res) => {
   try {
-    await usersCollection.insertOne(req.body);
+    await contentCollection.insertOne(req.body);
     res.send({
       success: true,
-      message:"Successfully created a post!"
-    })
+      message: "Successfully created a post!",
+    });
   } catch (error) {
     console.log(error);
-    res.send({success:false,error:error.message})
+    res.send({ success: false, error: error.message });
   }
-})
+});
+
+// ===> get users posts <==== //
+app.get("/content/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const result = await contentCollection.find({ email: email }).toArray();
+    res.send({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({ success: false, error: error.message });
+  }
+});
 
 //===Post Content===//
 app.get("/content", async (req, res) => {
   try {
-    const result=await usersCollection.find({}).toArray();
+    const result = await contentCollection.find({}).toArray();
     res.send({
       success: true,
-      data:result
-    })
+      data: result,
+    });
   } catch (error) {
     console.log(error);
-    res.send({success:false,error:error.message})
+    res.send({ success: false, error: error.message });
   }
-})
-
+});
 
 //=====Delete review=====//
 app.delete("/content/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
-     await usersCollection.deleteOne(query);
-    res.send({success:true,message:"Successfully deleted!"});
+    await contentCollection.deleteOne(query);
+    res.send({ success: true, message: "Successfully deleted!" });
   } catch (error) {
-      console.log(error)
-      res.send({
-        
+    console.log(error);
+    res.send({
       success: false,
       message: error.message,
     });
